b3c49ff3d7f52a52dae9d865517b50cf54772f0f
james31 <qunojames31@gmail.com>
2023-10-16 14:40:38 +0530

    first commit

--- src/lib/DeleteProduct.js
+++ src/lib/DeleteProduct.js
@@ -1,12 +1,10 @@
 "use client";
 
-// import { useRouter } from "next/router";
 import { useRouter } from 'next/navigation';
 
 export default function DeleteProduct(props) {
   const router = useRouter();
   const deleteRecord = async()=> {
-    // console.log(process.env.NEXT_PUBLIC_URL,"888");
     let response = await fetch(process.env.NEXT_PUBLIC_URL +"/api/products/"+props.id,{
         method:"delete"
     });
@@ -15,7 +13,6 @@ export default function DeleteProduct(props) {
     if (response.success) {
         alert("Product deleted")
         router.push("/products")
-        // router.refresh()
     }
   }
     return <button onClick={deleteRecord}>Delete</button>;
